9a3b269d908e5f62800cdcdb9f6a2352de9c6a86
davidearbola <davidearbola@gmail.com>
2024-04-22 16:10:06 +0200

    esercizio mail

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,28 @@
-console.log(`mail funziona`)
+// MAIL
+
+
+// Chiedi all’utente la sua email per poi controllare che sia nella lista di chi può accedere.
+
+
+// Stampa un messaggio SOLO SE l'utente è in lista.
+
+
+// creo array con lista di mail
+const adminMailingList = [
+    "prova@gmail.com",
+    "email@gmail.com",
+    "davide@gmail.com"
+];
+
+// chiedo all'utente di inserire la sua mail e la salverò in una variabile
+const userEmail = prompt(`inserisci email`);
+
+// controllo tramite ciclo e if che la email sia inserita o meno nell'array e stampo il risultato vero o falso
+
+for (let i = 0; i < adminMailingList.length; i++) {
+    const mailingSelected = adminMailingList[i];
+    if (mailingSelected == userEmail){
+        console.log(`La mail è nella lista`);
+    }
+}
+
