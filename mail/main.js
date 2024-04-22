f69b3a6505ddc6c9edf2cd80770781b74afc0dde
davidearbola <davidearbola@gmail.com>
2024-04-22 18:37:34 +0200

    esercizio mail finale

--- mail/main.js
+++ mail/main.js
@@ -7,7 +7,7 @@
 // Stampa un messaggio SOLO SE l'utente è in lista.
 
 
-// creo array con lista di mail
+// // creo array con lista di mail
 const adminMailingList = [
     "prova@gmail.com",
     "email@gmail.com",
@@ -17,12 +17,57 @@ const adminMailingList = [
 // chiedo all'utente di inserire la sua mail e la salverò in una variabile
 const userEmail = prompt(`inserisci email`);
 
-// controllo tramite ciclo e if che la email sia inserita o meno nell'array e stampo il risultato vero o falso
+// imposto variabile per l'email trovata su false
+let emailFound = false;
 
+// controllo tramite ciclo e if che la email sia inserita o meno nell'array 
 for (let i = 0; i < adminMailingList.length; i++) {
-    const mailingSelected = adminMailingList[i];
-    if (mailingSelected == userEmail){
-        console.log(`La mail è nella lista`);
-    }
+    // se la mail è nell'array riassegno la variabile emailFound come vera
+    if (adminMailingList[i] == userEmail){
+        emailFound = true;
+    } 
 }
+
+// controllo se la mail è stata trovata e stampo il risultato
+if (emailFound == true) {
+    console.log(`La tua mail è nell'elenco`)
+} else {
+    console.log(`La tua mail non è nell'elenco`)
+}
+
+
+
+
+
+
+
+
+
+
+// // Array di email fittizie
+// const elencoEmail = ['email1@example.com', 'email2@example.com', 'email3@example.com', 'email4@example.com', 'email5@example.com'];
+
+// // Chiedi all'utente di inserire la sua email
+// const emailUtente = prompt('Inserisci la tua email:');
+
+// // Variabile per tenere traccia se l'email è stata trovata
+// let emailTrovata = false;
+
+// // Ciclo per controllare se l'email dell'utente è contenuta nell'array
+// for (let i = 0; i < elencoEmail.length; i++) {
+//     if (elencoEmail[i] === emailUtente) {
+//         emailTrovata = true;
+//         // break; // Esci dal ciclo se trovi l'email
+//     }
+// }
+
+// // Verifica se l'email è stata trovata e stampa il risultato
+// if (emailTrovata) {
+//     console.log('La tua email è contenuta nell\'array.');
+// } else {
+//     console.log('La tua email non è contenuta nell\'array.');
+// }
+
+
+
 
